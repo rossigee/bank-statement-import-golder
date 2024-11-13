07c6a94a0cd6f19f6c2c59015ed96705b6c7385b
Ross Golder <ross@golder.org>
2024-11-13 19:51:23 +0700

    Bump version to 15.0.

--- account_statement_import_button/__manifest__.py
+++ account_statement_import_button/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Statement List Import Button',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'author':
         'Ross Golder',
     'website': 'https://golder.org/',
--- account_statement_update/__manifest__.py
+++ account_statement_update/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Custom bank statements handler',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'author':
         'Ross Golder',
     'website': 'https://golder.org/',
