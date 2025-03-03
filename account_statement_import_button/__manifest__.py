e4428d6462ae582c736cc8d667d78ef4d9b22be5
Ross Golder <ross@golder.org>
2025-03-03 13:36:52 +0700

    Update module versions.

--- account_statement_import_button/__manifest__.py
+++ account_statement_import_button/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Statement List Import Button',
-    'version': '15.0.1.0.0',
+    'version': '16.0.1.0.0',
     'author':
         'Ross Golder',
     'website': 'https://golder.org/',
